80ab02cfc9bd64e397c9149764ebcefd26cc9816
Shivashankar_2005348 <48704455+shivashankar2598@users.noreply.github.com>
2020-06-08 13:53:19 +0530

    Update HCF.c

--- HCF.c
+++ HCF.c
@@ -6,11 +6,11 @@ int main()
   printf("Enter number\n");
   scanf("%d %d",&num1,num2);
   int least=num1<num2?num1:num2;
- for(int i=2;i<=least;i++)
+ for(int i=1;i<=least;i++)
  {
      if((num1%i)==0 &&(num2%i)==0)
-        {hcf=i;
-        break;}
+        hcf=i;
+        
 
  }
  printf("The Hcf of %d and %d is %d",num1,num2,hcf);
