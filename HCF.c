1688cc87cb3bba7c9d4673f1656b27d90b0d69be
Shivashankar_2005348 <48704455+shivashankar2598@users.noreply.github.com>
2020-06-08 13:57:18 +0530

    Update HCF.c

--- HCF.c
+++ HCF.c
@@ -1,19 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  int num1=10,num2=20,hcf=1;
 
-#include <stdio.h> 
+  int least=num1<num2?num1:num2;
+ for(int i=1;i<=least;i++)
+ {
+     if((num1%i)==0 &&(num2%i)==0)
+        hcf=i;
 
-
-int gcd(int a, int b) 
-{ 
-	if (b == 0) 
-		return a; 
-	return gcd(b, a % b); 
-} 
-
- 
-int main() 
-{ 
-	int a = 99, b = 87; 
-	printf("GCD of %d and %d is %d ", a, b, gcd(a, b)); 
-	return 0; 
-} 
+ }
+ printf("The Hcf of %d and %d is %d",num1,num2,hcf);
+}
 
